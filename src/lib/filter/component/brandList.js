0e3bb3c53f9beb0d6e084ff749c92a41bf4f2d58
wesley.wang <wesley.wang@zerologix.com>
2018-08-02 11:45:24 +1000

    feature change

--- src/lib/filter/component/brandList.js
+++ src/lib/filter/component/brandList.js
@@ -6,7 +6,7 @@ import ActionRow from './actionRow';
 const BrandRow = styled.div`
   transition: max-height 300ms ease-in-out;
   overflow: hidden;
-  max-height: ${props => (props.collapse ? "80px" : "1500px")};
+  max-height: ${props => (props.collapse ? "85px" : "1500px")};
 `;
 
 const StyledCheckBox = styled.span`
@@ -16,7 +16,6 @@ const StyledCheckBox = styled.span`
 
 const StyledCol = styled(Col)`
     border: ${props => props.picked ? '1px solid grey' : '1px solid #f5f5f5'};
-    padding: 0 20px;
     margin: 15px 20px 0 0;
     text-align: center;
 `;
